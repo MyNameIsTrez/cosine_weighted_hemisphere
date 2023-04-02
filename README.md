225610c44c53d8d63df58a89d434ea4f8360c293
MyNameIsTrez <welfje@gmail.com>
2023-04-02 20:21:27 +0200

    Animation basics

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Render
-ffmpeg -i output/project/%06d.png -crf 40 C:/Users/welfj/Desktop/output.webm
+ffmpeg -i output/project/%06d.png -crf 1 C:/Users/welfj/Desktop/output.webm
+
+# API
+https://motioncanvas.io/api/2d/
--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,40 +1,96 @@
 import { makeScene2D } from '@motion-canvas/2d/lib/scenes';
-import { Circle } from '@motion-canvas/2d/lib/components';
+import { Grid, Rect, Line, Node } from '@motion-canvas/2d/lib/components';
 import { createRef } from '@motion-canvas/core/lib/utils';
-import { all } from '@motion-canvas/core/lib/flow';
-import { Img } from '@motion-canvas/2d/lib/components';
+import { all, delay, waitFor } from '@motion-canvas/core/lib/flow';
+import { CodeBlock, insert, lines } from '@motion-canvas/2d/lib/components/CodeBlock';
+// import { tween } from '@motion-canvas/core/lib/tweening';
+// import { DEFAULT, createSignal } from '@motion-canvas/core/lib/signals';
+import { Vector2 } from '@motion-canvas/core/lib/types';
 
-import cursorPng from "../../images/cursor.png";
-import handPng from "../../images/hand.png";
+const GRAY = '#2a2a2a';
+const LIGHT_GRAY = '#444444';
+const WHITE = '#F2F2F2'
+const RED = '#F22a2a'
 
 export default makeScene2D(function* (view) {
-	view.fill("#2a2a2a");
+	view.fill(GRAY);
 
-	const myCircle = createRef<Circle>();
+	const gridRef = createRef<Grid>();
+	view.add(<Grid ref={gridRef}
+		width={view.width}
+		height={view.height}
+		spacing={200}
+		stroke={LIGHT_GRAY}
+		lineWidth={4}
+		opacity={0}
+		lineDash={[15]}
+		// lineDashOffset={20}
+		// lineCap="round"
+	/>);
 
+	const normalRef = createRef<Line>();
+	const surfaceRef = createRef<Line>();
+	const group = createRef<Node>();
 	view.add(
-		<Circle
-			ref={myCircle}
-			// try changing these properties:
-			x={-800}
-			width={140}
-			height={140}
-			fill="#e13238"
-		/>,
+		<Node ref={group} y={200}>
+			<Line ref={normalRef}
+				points={[new Vector2(0, 0), new Vector2(0, -200)]}
+				stroke={RED}
+				lineWidth={10}
+				endArrow
+				end={0}
+			/>
+			<Line ref={surfaceRef}
+				points={[new Vector2(-400, 0), new Vector2(400, 0)]}
+				stroke={WHITE}
+				lineWidth={20}
+				lineCap={'round'}
+				opacity={0}
+			/>
+		</Node>
 	);
 
-	const cursorRef = createRef<Img>();
-	view.add(<Img ref={cursorRef} src={cursorPng} scale={0.3} x={10} />);
+	const rectRef = createRef<Rect>();
+	view.add(<Rect ref={rectRef}
+		fill={GRAY}
+		width={view.width}
+		height={400}
+		y={-840}
+	/>);
 
-	const handRef = createRef<Img>();
-	view.add(<Img ref={handRef} src={handPng} scale={0.75} x={100} />);
+	const codeRef = createRef<CodeBlock>();
+	yield view.add(<CodeBlock ref={codeRef}
+		language="tsx"
+		code={`ray.dir = plane.normal;`}
+		y={-740}
+	/>);
 
-	// yield* all(
-	// 	myCircle().position.x(300, 1).to(-300, 1),
-	// 	myCircle().fill('#e6a700', 1).to('#e13238', 1),
-	// );
+	yield* gridRef().opacity(1, 1.5);
+	yield* waitFor(0.6);
+	yield* surfaceRef().opacity(1, 1.5);
+	yield* waitFor(0.4);
+	yield* normalRef().end(1, 1.5);
+	yield* waitFor(0.6);
+	yield* group().rotation(30, 1.5).to(0, 1.5);
+	yield* waitFor(0.6);
 	yield* all(
-		cursorRef().scale(2.5, 1.5).to(2, 1.5),
-		cursorRef().absoluteRotation(90, 1.5).to(0, 1.5),
+		rectRef().position.y(-540, 2),
+		codeRef().position.y(-440, 2)
+	);
+	yield* waitFor(0.6);
+	yield* codeRef().edit(1.2)`ray.dir = plane.normal${insert(' + random()')};`;
+	yield* waitFor(0.6);
+	yield* codeRef().selection(lines(0, Infinity), 1);
+	yield* waitFor(0.6);
+	yield* codeRef().edit(1.2)`ray.dir = ${insert('normalize(')}plane.normal + random()${insert(')')};`;
+	yield* waitFor(0.6);
+	yield* codeRef().selection(lines(0, Infinity), 1);
+	yield* waitFor(0.6);
+	yield* all(
+		rectRef().position.y(-840, 0.6),
+		codeRef().position.y(-740, 0.6),
+		delay(0.1, normalRef().opacity(0, 0.6)),
+		delay(0.5, surfaceRef().opacity(0, 0.6)),
+		delay(0.9, gridRef().opacity(0, 0.6))
 	);
 });
